aaf94028f44eb48c504776daef1ce6c8d5e79911
Patrick Chow <PatrickChow0803@gmail.com>
2019-10-06 17:05:04 -0400

    Added an alertDialog and placed a sample image in details activity. (Part 4)

--- app/src/main/java/com/patrickchow/patsecommerce/ProductDetails.kt
+++ app/src/main/java/com/patrickchow/patsecommerce/ProductDetails.kt
@@ -1,6 +1,8 @@
 package com.patrickchow.patsecommerce
 
+import android.content.DialogInterface
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.product_details.*
 
@@ -11,5 +13,21 @@ class ProductDetails : AppCompatActivity(){
 
         val title = intent.getStringExtra("title")
         tv_title_details.text = title
+
+        //Creates an alert dialog
+        btn_availability.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setMessage("$title is in stock!")
+
+                //Gives the dialog an OK text.  You can alt+enter convert to lambda but I'll be keeping it like this just incase.
+                .setPositiveButton("OK", object:DialogInterface.OnClickListener{
+                    override fun onClick(p0: DialogInterface?, p1: Int) {
+
+                    }
+
+                })
+                .create()
+                .show()
+        }
     }
 }
