fd27299d49c5fb1069f7073d0d910bc47f3c1bea
Patrick Chow <PatrickChow0803@gmail.com>
2019-10-06 13:59:38 -0400

    Added the base for a RecyclerView

---
+++ app/src/main/java/com/patrickchow/patsecommerce/ProductsAdapter.kt
@@ -0,0 +1,22 @@
+package com.patrickchow.patsecommerce
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ProductsAdapter(private val products: ArrayList<Product>) : RecyclerView.Adapter<ProductsAdapter.ViewHolder>(){
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductsAdapter.ViewHolder {
+
+    }
+
+    override fun getItemCount(): Int {
+    }
+
+    override fun onBindViewHolder(holder: ProductsAdapter.ViewHolder, position: Int) {
+    }
+
+    class ViewHolder {
+
+    }
+}
