ba04bb3bd42ce07b8a116d78264aaa389a8c27aa
Patrick Chow <PatrickChow0803@gmail.com>
2019-10-10 11:23:10 -0400

    Added Navigation (5/6)

--- app/src/main/java/com/patrickchow/patsecommerce/MainActivity.kt
+++ app/src/main/java/com/patrickchow/patsecommerce/MainActivity.kt
@@ -2,6 +2,10 @@ package com.patrickchow.patsecommerce
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.patrickchow.patsecommerce.model.Product
@@ -12,6 +16,31 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        toolbar.title= "Clothing"
+
+        //Makes the burger on the top left of toolbar
+        val toggle = ActionBarDrawerToggle(this, drawer_layout,
+            toolbar, R.string.open_drawer, R.string.close_drawer)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        //Sets click listener to the navigation
+        nav_view.setNavigationItemSelectedListener {
+            it.isChecked = true
+            when(it.itemId){
+                R.id.clothes_shirt -> {
+                    Toast.makeText(this, "Shirts", Toast.LENGTH_SHORT).show()
+                }
+                R.id.cloths_pants -> {
+                    Toast.makeText(this, "Pants", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            drawer_layout.closeDrawers()
+
+            true
+        }
 
         val products = arrayListOf<Product>()
 
@@ -24,4 +53,24 @@ class MainActivity : AppCompatActivity() {
             adapter = ProductsAdapter(products)
         }
     }
+
+    // Inflates the menu_sign and place it to the top right
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_sign, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    // Gives the item selected functionality
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.sign_in -> {
+                Toast.makeText(this, "Signed In", Toast.LENGTH_SHORT).show()
+            }
+            R.id.sign_out -> {
+                Toast.makeText(this, "Signed Out", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
+
